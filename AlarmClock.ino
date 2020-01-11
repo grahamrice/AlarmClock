6a925788766efb22f206abf7a946662bfdc72793
Graham Rice <grahamrice1806@gmail.com>
2020-01-11 16:12:36 +0000

    various debugging done to button presses for consistent operation. Display now turns off correctly. Need to test alarm and time setting functionality before working on the i2c output for the display

--- AlarmClock.ino
+++ AlarmClock.ino
@@ -77,9 +77,10 @@ uint8_t button_last = 0;
 #define BUTTON_ALARM_H 0x80
 #define BUTTON_ALL_H   0xF0
 
-#define TOUCH_THRESH 2
+#define TOUCH_THRESH 3
 #define HOLD_THRESH  12
-#define HOLD_HOLD    14
+#define HOLD_HOLD1   15
+#define HOLD_HOLD2   14
 
 int alarmHours = 0, alarmMinutes = 0;  // Holds the current alarm time
 int snooze_hours = 0, snooze_minutes = 0;
@@ -119,19 +120,25 @@ uint8_t sec_flash_counter = 0;
 
 void print_number(int n, char * buf)
 {
-  if(n < 0) sprintf(buf,'  ');
-  else if(n > 99) sprintf(buf,'**');
-  else            sprintf(buf,'%02d',n);      
+  if(n < 0)       sprintf(buf,"  ");
+  else if(n > 99) sprintf(buf,"**");
+  else            sprintf(buf,"%02d",n);       
 }
 
-void display_time(int H, int M, int S)
+void display_time(int H, int M, int S, bool p)
 {
   /* Will display numbers between 0 and 99 */
   /* Negative values mean nothing is displayed */
-  int temp[3] = {H,M,S};
-  char _str_buffer[7];  //6 chars and a null char...
-  for(int i = 0; i < 3; i++)  print_number(temp[i],&_str_buffer[i*2]); 
-  Serial.write(_str_buffer,7);
+  int temp[3];
+  char _str_buffer[8];  //6 chars and a null char...
+  temp[0] = H;
+  temp[1] = M;
+  temp[2] = S;
+  
+  for(int i = 0; i < 3; i++)  print_number(temp[i],_str_buffer + (i*2)); 
+  
+  if(p) Serial.println(_str_buffer);
+  //Serial.println("");
  // LEDdisplay.FillTextBuffer(_str_buffer);
 }
 
@@ -139,16 +146,31 @@ void display_snooze_time(int ss)
 {
   char _str_buffer[7];
   sprintf(_str_buffer,"SNZ %02d",ss);
-Serial.write(_str_buffer,7);
- // LEDdisplay.FillTextBuffer(_str_buffer);
+  Serial.write(_str_buffer,7);
+  Serial.println(" ");
+  // LEDdisplay.FillTextBuffer(_str_buffer);
 }
 
 void display_alarm_status(bool s){
   char _str_buffer[7];
   if(s) sprintf(_str_buffer,"al  on");
   else  sprintf(_str_buffer,"al off");
-Serial.write(_str_buffer,7);
+  Serial.write(_str_buffer,7);
+  Serial.println(" ");
   //LEDdisplay.FillTextBuffer(_str_buffer);
+}
+
+void DisplayDateTime (bool h, bool m, bool s, bool p)
+{
+  static int old_secs = 0;
+// We show the current date and time
+  DateTime now = rtc.now();
+
+  if(old_secs != now.second()) old_secs = now.second();
+  else p = false;
+  
+  display_time( (h) ? now.hour() : -1, (m) ? now.minute() : -1, (s) ? now.second() : -1, p);
+  
 }
 
 void update_display()
@@ -171,36 +193,36 @@ void update_display()
   switch(current_display){
     case DISPLAY_OFF: //probs do nothing
                      break;
-    case TIME_CURRENT:  DisplayDateTime (true, true, true);
+    case TIME_CURRENT:  DisplayDateTime (true, true, true, true);
                         if(sec_flash_counter == 2) UI_Leds |= (RAW_LEDS_1 | RAW_LEDS_3); 
                         UI_Leds |= (alarm_status & ALARM_SET) ? RAW_LEDS_W : 0;
                         break;
                         
-    case TIME_SET_H:   if(sec_flash_counter < 3) DisplayDateTime (true, true, true);
-                       else                      DisplayDateTime (false, true, true);
+    case TIME_SET_H:   if(sec_flash_counter < 3) DisplayDateTime (true, true, true, true);
+                       else                      DisplayDateTime (false, true, true,true );
                        if(sec_flash_counter == 2) (RAW_LEDS_1 | RAW_LEDS_3);
                        UI_Leds |= (alarm_status & ALARM_SET) ? RAW_LEDS_W : 0;
                        UI_Leds |= RAW_LEDS_S;
                        break;
                        
-    case TIME_SET_M:   if(sec_flash_counter < 3) DisplayDateTime (true, true, true);
-                       else                      DisplayDateTime (true, false, true);
+    case TIME_SET_M:   if(sec_flash_counter < 3) DisplayDateTime (true, true, true, true);
+                       else                      DisplayDateTime (true, false, true, true);
                        if(sec_flash_counter == 2) (RAW_LEDS_1 | RAW_LEDS_3);
                        UI_Leds |= (alarm_status & ALARM_SET) ? RAW_LEDS_W : 0;
                        UI_Leds |= RAW_LEDS_S;
                        break;
                        
-    case TIME_ALARM_H: if(sec_flash_counter == 2) display_time(-1,alarmMinutes, -1);
-                       else display_time(alarmHours,alarmMinutes, -1);
+    case TIME_ALARM_H: if(sec_flash_counter == 2) display_time(-1,alarmMinutes, -1, true);
+                       else display_time(alarmHours,alarmMinutes, -1, true);
                        UI_Leds |= (alarm_status & ALARM_SET) ? RAW_LEDS_W : 0;
                        break;
 
-    case TIME_ALARM_M: if(sec_flash_counter == 2) display_time(alarmHours,-1, -1);
-                       else display_time(alarmHours,alarmMinutes, -1);
+    case TIME_ALARM_M: if(sec_flash_counter == 2) display_time(alarmHours,-1, -1, true);
+                       else display_time(alarmHours,alarmMinutes, -1, true);
                        UI_Leds |= (alarm_status & ALARM_SET) ? RAW_LEDS_W : 0;
                        break;
 
-    case TIME_SNOOZE: if(sec_flash_counter == 2) display_time(-1,-1, snooze_time);
+    case TIME_SNOOZE: if(sec_flash_counter == 2) display_time(-1,-1, snooze_time, true);
                       else display_snooze_time(snooze_time);
                       if(sec_flash_counter < 3) UI_Leds |= RAW_LEDS_W;
                       break;
@@ -243,51 +265,93 @@ void sound_alarm()
 
 int check_buttons()
 {
-  uint8_t buttons = 0, mask = 0;
-  int result = 0;
+  uint8_t buttons = 0, mask = 0, j;
+  int result[2] = {0,0}, res = 0;
+  static uint8_t press_complete = 0;
+  bool holdholdflag = false;
+  uint8_t holdholdtemp[4] = {0,0,0,0};
+
+  if(press_complete > 0)
+  {
+    press_complete--; /* no button press immediately after a previous */
+    return 0;
+  }
 
   if(digitalRead(BUTTON1)) buttons |= BUTTON_MENU;
   if(digitalRead(BUTTON2)) buttons |= BUTTON_DOWN;
   if(digitalRead(BUTTON3)) buttons |= BUTTON_UP;
   if(digitalRead(BUTTON4)) buttons |= BUTTON_ALARM;
   
-  for(int j = 0; j < 4; j++)
+  for(j = 0; j < 4; j++)
   {
     mask = 1 << j;
-    if((buttons & mask) == mask) button_time[j]++; //if button pressed, increment counter
+    if((buttons & mask) == mask)
+    {
+      button_time[j]++; //if button pressed, increment counter
+      
+      if(button_time[j] == -1) //still pressed and we've reached the hold threshold again, issue hold command.
+      {
+        result[0] |= (mask << 4);
+        button_time[j] = (-1) * HOLD_HOLD2;
+        holdholdflag = true;
+      }
+    }  
     else if(button_time[j] != 0)                    //if not has it been pressed before?
     {
-      if(button_time[j] > HOLD_THRESH ){ result |= (mask << 4); //we've been holding it, so issue that command
-                                         button_time[j] = 0; } //don't hold and touch at the same time
-      if(button_time[j] > TOUCH_THRESH)  result |= mask; //(else) we've touched it, so issue that command
+      if(button_time[j] > HOLD_THRESH ) result[0] |= (mask << 4); //we've been holding it, so issue that command
+                                          //don't hold and touch at the same time
+      else if(button_time[j] > TOUCH_THRESH)  result[1] |= mask; //(else) we've touched it, so issue that command
 
-      button_time[j] = 0; //button no longer pressed, so clear hold time. 
+      else button_time[j] = 0; //button no longer pressed, so clear hold time. 
     }
 
-    if(button_time[j] > HOLD_HOLD) //still pressed and we've reached the hold threshold
+    if(button_time[j] > HOLD_HOLD1) //still pressed and we've reached the hold threshold
     {
-      result |= (mask << 4);
-      button_time[j]= (-1) * HOLD_HOLD; //if we're going to carry on holding then we'll count upwards so we don't trigger a touch again;
+      result[0] |= (mask << 4);
+      button_time[j]= (-1) * HOLD_HOLD2; //if we're going to carry on holding then we'll count upwards so we don't trigger a touch again;
+      holdholdflag = true;
+    }else{
+      if(button_time[j] > HOLD_HOLD2) holdholdtemp[j]= (-1) * HOLD_HOLD2;
     }
 
-    if(button_time == -1) //still pressed and we've reached the hold threshold again, issue hold command.
-    {
-      result |= (mask << 4);
-      button_time[j] = (-1) * HOLD_HOLD;
-    }
-      
   }
 
-  if(result != 0)
+  /* If one hold has been released but not all at that moment, return all relevant bits instead of just those released */
+  /* i.e. give the benefit of the doubt, otherwise we need to add some kind of release timer */
+  /* Hold threshold is greater priority than touch threshold */
+  if(result[0] != 0)
   {
-    Serial.print("Button press: ");
-    Serial.println(result);
+    for(j = 0; j < 4; j++)
+    {
+      mask = 1 << j;
+      if(button_time[j] > HOLD_THRESH) res |= (mask << 4);
+      if(button_time[j] < 0) res |= (mask << 4);
+      if(holdholdtemp[j] < 0){
+        res |= (mask << 4);
+        button_time[j] = holdholdtemp[j];
+      }
+    }
+  }
+  else if(result[1] != 0)
+  {
+    for(j = 0; j < 4; j++)
+    {
+      mask = 1 << j;
+      if(button_time[j] > TOUCH_THRESH) res |= mask;
+    }
   }
 
-  return result;
+  if(res != 0)
+  {
+    if(!holdholdflag) for(j = 0; j < 4; j++) button_time[j] = 0;
+    Serial.print("\n\nButton press: ");
+    Serial.println(res);
+    press_complete = TOUCH_THRESH;
+  }
+
+  return res;
 }
 
-//ISR(_VECTOR(14)) //interrupt every 50ms
 ISR(TIMER1_OVF_vect)
 {
   TCNT1 = TIMER_RESET;
@@ -373,11 +437,10 @@ void loop()
   int buttons = 0;
   static int menu_timeout = -1;
   static int led_counter = 0;
-  static int bc, dc;
   
-  if((interrupt_flags & FLAGS_DISPLAY) == FLAGS_DISPLAY) { update_display(); dc++;}
+  if((interrupt_flags & FLAGS_DISPLAY) == FLAGS_DISPLAY) update_display(); 
 
-  if((interrupt_flags & FLAGS_BUTTONS) == FLAGS_BUTTONS) { buttons = check_buttons(); bc++;}
+  if((interrupt_flags & FLAGS_BUTTONS) == FLAGS_BUTTONS) buttons = check_buttons();
 
   if((interrupt_flags & FLAGS_SOUND) == FLAGS_SOUND) sound_alarm();
   
@@ -387,14 +450,12 @@ void loop()
 
   if( buttons != 0 )
   {
-    Serial.print("\nButtons:");
-    Serial.print(buttons);
     DateTime current = rtc.now();
     if((alarm_status & ALARM_TRG) == ALARM_TRG)
     {
       if(menu_mode == MENU_OFF) menu_mode = MENU_NONE; //alarm = display back on
       
-      if((buttons == BUTTON_ALL)||(buttons == BUTTON_ALL_H)) alarm_status &= ALARM_SET;
+      if((threepress(buttons,0))||(threepress(buttons,4))) alarm_status &= ~ALARM_SET;
       else if((buttons == BUTTON_MENU)||(buttons == BUTTON_DOWN)||(buttons == BUTTON_UP)||(buttons == BUTTON_ALARM))
       { //go into snooze mode
         if((alarm_status & ALARM_SNZ) == 0)
@@ -408,7 +469,7 @@ void loop()
       }
     }else if((alarm_status & ALARM_SNZ) == ALARM_SNZ) 
     { //turn off when not sounding
-      if(buttons == BUTTON_ALL_H)
+      if(threepress(buttons,4))
       {
         snooze_hours = 0;
         snooze_minutes = 0;
@@ -418,7 +479,7 @@ void loop()
     {
       switch(menu_mode)
       {
-              case MENU_OFF  :   if(buttons == BUTTON_ALL) menu_mode = MENU_NONE;
+              case MENU_OFF  :   if(threepress(buttons,0)) menu_mode = MENU_NONE;
                                  break; 
               case MENU_NONE :   if(buttons == BUTTON_MENU_H)
                                   {
@@ -432,18 +493,17 @@ void loop()
                                   {
                                     menu_mode = MENU_SAL_HH;
                                     menu_timeout = 7000;
-                                  }else if(buttons == BUTTON_ALL) menu_mode = MENU_OFF;
+                                  }else if(threepress(buttons,0)) menu_mode = MENU_OFF;
                                   break;
               case MENU_SET_HH : if(buttons == BUTTON_MENU)
                                   {
                                     menu_mode = MENU_SET_MM;
                                   }else if(buttons == BUTTON_DOWN)
                                   {
-                                    //ffs there is no nice rtc library, do this properly later
-                                    rtc.adjust(current); 
+                                    rtc.adjust(current-TimeSpan(3600)); 
                                   }else if(buttons == BUTTON_UP)
                                   {
-                                    
+                                    rtc.adjust(current+TimeSpan(3600)); 
                                   }
                                   menu_timeout = 7000; 
                                   break;
@@ -453,13 +513,12 @@ void loop()
                                     menu_timeout = -1; 
                                   }else if(buttons == BUTTON_DOWN)
                                   {
-                                    //ffs there is no nice rtc library, do this properly later
-                                    rtc.adjust(current); 
-                                    menu_timeout = 7000; 
+                                    rtc.adjust(current-TimeSpan(60)); 
                                   }else if(buttons == BUTTON_UP)
                                   {
-                                    menu_timeout = 7000; 
+                                    rtc.adjust(current+TimeSpan(60));                                     
                                   }
+                                  menu_timeout = 7000; 
                                   break;
               case MENU_SAL_HH : if((buttons == BUTTON_MENU)||(buttons == BUTTON_ALARM))
                                   {
@@ -467,9 +526,11 @@ void loop()
                                   }else if(buttons == BUTTON_DOWN)
                                   {
                                     alarmHours--;
+                                    if(alarmHours < 0) alarmHours = 23;
                                   }else if(buttons == BUTTON_UP)
                                   {
                                     alarmHours++;
+                                    if(alarmHours > 23) alarmHours = 0;
                                   }
                                   menu_timeout = 5000;
                                   break;
@@ -479,9 +540,11 @@ void loop()
                                   }else if(buttons == BUTTON_DOWN)
                                   {
                                     alarmMinutes--;
+                                    if(alarmMinutes < 0) alarmMinutes = 59;
                                   }else if(buttons == BUTTON_UP)
                                   {
                                     alarmMinutes++;
+                                    if(alarmMinutes > 59) alarmMinutes = 0;
                                   }
                                   menu_timeout = 5000;
                                   break;
@@ -492,9 +555,11 @@ void loop()
                                   }else if(buttons == BUTTON_DOWN)
                                   {
                                     snooze_time--;
+                                    if(snooze_time < 1) snooze_time = 1;
                                   }else if(buttons == BUTTON_UP)
                                   {
                                     snooze_time++;
+                                    if(snooze_time > 60) snooze_time = 60;
                                   }
                                   menu_timeout = 5000;
                                   break;
@@ -506,55 +571,48 @@ void loop()
               default : menu_mode = MENU_NONE;         
       }
     }
+    Serial.print("Alarm Status: ");
+    Serial.println(alarm_status);
+    Serial.print("Menu Status: ");
+    Serial.println(menu_mode);
   }
 
   
   if(menu_timeout >= 0)
   {
     if(menu_timeout == 0) menu_mode = MENU_NONE;
-    
-    switch(menu_mode)
-    {
-      case MENU_OFF    : current_display = DISPLAY_OFF;
-                         break;
-      case MENU_NONE   : current_display = TIME_CURRENT;
-                         break;
-      case MENU_SET_HH : current_display = TIME_SET_H;
-                         break;
-      case MENU_SET_MM : current_display = TIME_SET_M;
-                         break;
-      case MENU_SAL_HH : current_display = TIME_ALARM_H;
-                         break;
-      case MENU_SAL_MM : current_display = TIME_ALARM_M;
-                         break;
-      case MENU_SAL_SN : current_display = TIME_SNOOZE;
-                         break;
-      case MENU_SAL_TN : current_display = TIME_AL_TONE;
-                         break;
-      case MENU_DSP_AL : current_display = TIME_ALARM_STATUS;
-                         break;    
-    }
+    if(menu_timeout == 0) Serial.println("menu timed out");
+    menu_timeout--;
   }
 
-  menu_timeout--;
+  switch(menu_mode)
+  {
+    case MENU_OFF    : current_display = DISPLAY_OFF;
+                        break;
+    case MENU_NONE   : current_display = TIME_CURRENT;
+                        break;
+    case MENU_SET_HH : current_display = TIME_SET_H;
+                        break;
+    case MENU_SET_MM : current_display = TIME_SET_M;
+                        break;
+    case MENU_SAL_HH : current_display = TIME_ALARM_H;
+                        break;
+    case MENU_SAL_MM : current_display = TIME_ALARM_M;
+                        break;
+    case MENU_SAL_SN : current_display = TIME_SNOOZE;
+                        break;
+    case MENU_SAL_TN : current_display = TIME_AL_TONE;
+                        break;
+    case MENU_DSP_AL : current_display = TIME_ALARM_STATUS;
+                        break;    
+  }
+
   delay(1);
 
   if(++led_counter == 500)
   {
     led_counter = 0;
     digitalWrite(LED,!digitalRead(LED));
-  }
-}
-
-void DisplayDateTime (bool h, bool m, bool s)
-{
-  static int old_secs = 0;
-// We show the current date and time
-  DateTime now = rtc.now();
-  display_time( (h) ? now.hour() : -1, (m) ? now.minute() : -1, (s) ? now.second() : -1);
-  if(old_secs != now.second()){
-    Serial.println(now.second());
-    old_secs = now.second();
   }
 }
 
@@ -578,4 +636,17 @@ void check_alarm()
   {
     alarm_status |= ALARM_TRG;
   }
+}
+
+bool threepress(int b, int uplo)
+{
+  uint8_t c = 0;
+  uint8_t mask = 0;
+  for(int i = 0; i < 4; i++)
+  {
+    mask = 1 << (i + uplo);
+    if((b & mask) == mask) c++;
+  }
+  if(c > 2) return true;
+  else return false;
 }
